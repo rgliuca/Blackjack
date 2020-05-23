805f4f3f5acf7427093a2ed605c21bf5b147ba67
rgliuca <44307228+rgliuca@users.noreply.github.com>
2020-05-23 16:03:34 -0700

    Add files via upload

--- blackjack_OOP_student_stubs.py
+++ blackjack_OOP_student_stubs.py
@@ -13,34 +13,79 @@ class Card:
   line_bottom='└─────────┘\t'
 
   def __init__(self, value):
+    # This function initializes the actual card value (a value from 0 to 51)
     pass
 
   def get_suit_image(self):
+    # This function returns one of the card suit image, depends on the card's value
+    # You'll have to compute the suit based on the value of the card and 
+    # then return one of the element from the card_suit_image list:
+    # '♠', '♥', '♣', '♦'
     pass
 
   def value(self):
+    # This function computes the value of the card
+    # The value of the card is just an integer value from 1 to 13
+    # 1 is for Ace, 2 for 2, ... 12 for Queen and 13 is for King
     pass
 
   def get_rank_image(self):
+    # This function computes the rank based on the card value and returns a
+    # string of the following form:
+    # "A░", "2░","3░","4░","5░","6░","7░","8░","9░","10","J░","Q░","K░"
     pass
 
   def print_card(self, print_back=False):
+    # This functions prints the poker card (text) image 
+    # Example: 
+    # ┌─────────┐
+    # │░░░░░░░░░│
+    # │░J░░░░░░░│
+    # │░░░░░░░░░│
+    # │░░░░♥░░░░│
+    # │░░░░░░░░░│
+    # │░░░░░░░░░│
+    # │░░░░░░░░░│
+    # └─────────┘
+    # if the input parameter print_back is True, then you need to print
+    # a blank card (the back of a poker card)
     pass
 
   def get_card_image(self, print_back=False):
+    # This function returns a list of string
+    # The list contains each line of the poker card's text image
+    # BTW, the Card.print_card() function can call this to get the image text list
+    # and then print each line out using a loop
+    # Example:
+    # ['┌─────────┐',
+    #  '│░░░░░░░░░│',
+    #  '│░J░░░░░░░│',
+    #  '│░░░░░░░░░│',
+    #  '│░░░░♥░░░░│',
+    #  '│░░░░░░░░░│',
+    #  '│░░░░░░░░░│',
+    #  '│░░░░░░░░░│',
+    #  '└─────────┘']
     pass
 
 class Card_Deck:
   def __init__(self):
+    # This function initializes the deck
+    # You should create a list of 52 cards in sequence from 0 to 51
     pass
 
   def shuffle(self):
+    # This function shuffles the deck of cards
+    # Hint: just shuffle the list of Cards (object)
     pass
 
   def deal_card(self):
+    # This function deals a card from the deck 
+    # Just pot a card from the list and return it 
     pass
 
   def get_cards_remaining(self):
+    # This function returns the # of cards left in the deck
     pass
 
 
